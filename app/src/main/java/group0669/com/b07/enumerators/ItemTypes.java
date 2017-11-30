e44325cf6054d1a4cea0292be4ed8eba13737dd7
waqartay <waqartay@UTORONTO.CA>
2017-11-30 22:33:28 +0000

    Relocated the backend code!

---
+++ app/src/main/java/group0669/com/b07/enumerators/ItemTypes.java
@@ -0,0 +1,5 @@
+package com.b07.enumerators;
+
+public enum ItemTypes {
+  FISHING_ROD, HOCKEY_STICK, SKATES, RUNNING_SHOES, PROTEIN_BAR;
+}
---
+++ app/src/main/java/group0669/com/b07/store/SaleImpl.java
@@ -0,0 +1,58 @@
+package com.b07.store;
+
+import com.b07.inventory.Item;
+import com.b07.users.User;
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+
+public class SaleImpl implements Sale {
+  private int id;
+  private User user;
+  private BigDecimal price;
+  private HashMap<Item, Integer> itemMap = new HashMap<Item, Integer>();
+
+  @Override
+  public int getId() {
+    return this.id;
+  }
+
+  @Override
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  @Override
+  public User getUser() {
+    return user;
+  }
+
+  @Override
+  public void setUser(User user) {
+    this.user = user;
+
+  }
+
+  @Override
+  public BigDecimal getTotalPrice() {
+    return price;
+  }
+
+  @Override
+  public void setTotalPrice(BigDecimal price) {
+    this.price = price;
+  }
+
+  @Override
+  public HashMap<Item, Integer> getItemMap() {
+    return itemMap;
+  }
+
+  @Override
+  public void setItemMap(HashMap<Item, Integer> itemMap) {
+    this.itemMap = itemMap;
+  }
+
+
+
+}
