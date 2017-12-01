38baaac61745570653b97dfc7268050766302b29
waqartay@UTORONTO.CA <waqartay@UTORONTO.CA@68c64ce6-14de-46c5-bdfa-c4909d9eba32>
2017-12-01 19:21:50 +0000

    Added the class for android update helper methods

---
+++ projectPhase3/app/src/main/java/com/b07/database/helper/android/DatabaseAndroidUpdateHelper.java
@@ -0,0 +1,17 @@
+package com.b07.database.helper.android;
+
+import android.content.Context;
+
+import com.b07.database.DatabaseDriverAndroid;
+
+/**
+ * Created by Tayyab on 2017-12-01.
+ */
+
+public class DatabaseAndroidUpdateHelper extends DatabaseDriverAndroid {
+    public DatabaseAndroidUpdateHelper(Context context) {
+        super(context);
+    }
+
+
+}
