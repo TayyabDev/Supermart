24b337e11a8aa73ad6c99b2897adbff7a4993633
waqartay <waqartay@UTORONTO.CA>
2017-12-02 00:00:01 +0000

    Added ability for user to see list of all items in st ore when he wants to go add an item.

--- app/src/main/java/group0669/com/example/supermart/AddItemActivity.java
+++ app/src/main/java/group0669/com/example/supermart/AddItemActivity.java
@@ -4,16 +4,45 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.b07.database.helper.DatabaseSelectHelper;
+import com.b07.database.helper.android.DatabaseAndroidSelectHelper;
+import com.b07.inventory.Item;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddItemActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button buttonSave;
+    ListView listItems;
+    ListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        // initialize the list of items
+        DatabaseAndroidSelectHelper sel = new DatabaseAndroidSelectHelper(this);
+        // get list of items
+        List<Item> items = sel.getAllItemsHelper();
+
+        // get a list with name of each item
+        List<String> itemsNames = new ArrayList<>();
+        for(Item item : items){
+            itemsNames.add(item.getName());
+        }
+        listItems = findViewById(R.id.listItems);
+        adapter = new ArrayAdapter<String>(listItems.getContext(), android.R.layout.simple_list_item_1, itemsNames);
+        listItems.setAdapter(adapter);
+
 
         buttonSave = (Button) findViewById(R.id.buttonSave);
 
