9200d804491b8a845c2b86704b23b9fa4adc7184
waqartay@UTORONTO.CA <waqartay@UTORONTO.CA@68c64ce6-14de-46c5-bdfa-c4909d9eba32>
2017-12-03 07:05:05 +0000

    Added functionality for customer to add items and view his shopping cart.

--- projectPhase3/app/src/main/java/group0669/com/example/supermart/CustomerActivity.java
+++ projectPhase3/app/src/main/java/group0669/com/example/supermart/CustomerActivity.java
@@ -15,15 +15,22 @@ import android.widget.Toast;
 
 import com.b07.database.helper.android.DatabaseAndroidSelectHelper;
 import com.b07.exceptions.CustomerNotLoggedInException;
+import com.b07.exceptions.DatabaseInsertException;
 import com.b07.exceptions.InvalidIdException;
+import com.b07.exceptions.InvalidInputException;
+import com.b07.exceptions.InvalidQuantityException;
 import com.b07.exceptions.InvalidRoleException;
+import com.b07.exceptions.ItemNotFoundException;
+import com.b07.inventory.Item;
 import com.b07.store.ShoppingCart;
 import com.b07.users.Customer;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class CustomerActivity extends AppCompatActivity implements View.OnClickListener{
 
+    ShoppingCart sc;
     Button buttonRestoreShoppingCart;
     Button buttonAddItem;
     Button buttonRemoveItem;
@@ -35,12 +42,12 @@ public class CustomerActivity extends AppCompatActivity implements View.OnClickL
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
 
-        // get the intent informationn from login activity
+        // get the intent information from login activity
         Bundle loginData = getIntent().getExtras();
         if(loginData == null){
            return;
         }
-        // get the userid from the bundle
+        // get the user ID from the bundle
         int userId = loginData.getInt("userId");
 
         // get user object given the id
@@ -58,19 +65,12 @@ public class CustomerActivity extends AppCompatActivity implements View.OnClickL
         welcomeUser.setText("Welcome " + customer.getName() + "!");
 
         // make the shopping cart
-        /**
-         *
         try {
-            ShoppingCart sc = new ShoppingCart(customer);
+            sc = new ShoppingCart(customer, this);
         } catch (CustomerNotLoggedInException e) {
             e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
-         */
-
-
-
+        System.out.println(sc.getItems());
 
         buttonRestoreShoppingCart = (Button) findViewById(R.id.buttonRestoreShoppingCart);
         buttonRestoreShoppingCart.setOnClickListener(this);
@@ -102,7 +102,22 @@ public class CustomerActivity extends AppCompatActivity implements View.OnClickL
                 break;
 
             case R.id.buttonCheckShoppingCart:
-                startActivity(new Intent(this, CheckShoppingCartActivity.class));
+                Intent intentCheckShoppingCart = new Intent(this, CheckShoppingCartActivity.class);
+
+                List<Item> items = sc.getItems();
+                // create two new arrays for item names and quantities
+                String [] itemNames = new String [items.size()];
+                int [] itemQuantities = new int [items.size()];
+
+                // populate each array with its respective data
+                for(int i = 0; i < itemNames.length; i++){
+                itemNames[i] = items.get(i).getName();
+                itemQuantities[i] = sc.getQuantity(items.get(i));
+            }
+
+                intentCheckShoppingCart.putExtra("itemNames", itemNames);
+                intentCheckShoppingCart.putExtra("itemQuantities", itemQuantities);
+                startActivity(intentCheckShoppingCart);
                 break;
 
             case R.id.buttonCheckOut:
@@ -117,8 +132,33 @@ public class CustomerActivity extends AppCompatActivity implements View.OnClickL
         super.onActivityResult(requestCode, resultCode, data);
         // if request code is 1 then add item to cart
         if(requestCode == 1 && resultCode == RESULT_OK){
-            System.out.println(data.getStringExtra("itemName") + data.getIntExtra("quantity", 0));
+            // get item name
+            String itemName = data.getStringExtra("itemName");
+            // get quantity
+            int quantity = data.getIntExtra("quantity", 0);
 
+            // search for item and add it to cart once found
+            DatabaseAndroidSelectHelper sel = new DatabaseAndroidSelectHelper(this);
+            List<Item> itemList = sel.getAllItemsHelper();
+            for(Item item : itemList){
+                // add item to cart with quantity
+                if(item.getName().equals(itemName)){
+                    try {
+                        sc.addItem(item, quantity, this);
+                        Toast.makeText(this, "Item added to cart.", Toast.LENGTH_SHORT).show();
+                    } catch (SQLException e) {
+                        Toast.makeText(this, "Error with SQL", Toast.LENGTH_SHORT).show();
+                    } catch (ItemNotFoundException e) {
+                        Toast.makeText(this, "Item could not be found.", Toast.LENGTH_SHORT).show();
+                    } catch (InvalidQuantityException e) {
+                        Toast.makeText(this, "Quantity specified was invalid.", Toast.LENGTH_SHORT).show();
+                    } catch (DatabaseInsertException e) {
+                        Toast.makeText(this, "Error inserting.", Toast.LENGTH_SHORT).show();
+                    } catch (InvalidInputException e) {
+                        Toast.makeText(this, "Error in input", Toast.LENGTH_SHORT).show();
+                    } break;      // break from loop since we are done
+                }
+            }
         }
     }
 }
