86444d9ebcebf640eefb8b766fefb5043ad49cd0
hongsuxi <hongsuxi@UTORONTO.CA>
2017-12-04 22:59:33 +0000

    add GetUserInformationActivity.class UserInformationActivity.class and their layouts

---
+++ app/src/main/java/group0669/com/example/supermart/UserInformationActivity.java
@@ -0,0 +1,13 @@
+package group0669.com.example.supermart;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class UserInformationActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_user_information);
+  }
+}
