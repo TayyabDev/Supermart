169b4a984ac13c210855d689d9e074d31e507e38
hongsuxi <hongsuxi@UTORONTO.CA>
2017-12-04 21:03:21 +0000

    Make the image silder slide automaticly

--- app/src/main/java/group0669/com/example/supermart/MainActivity.java
+++ app/src/main/java/group0669/com/example/supermart/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -25,6 +27,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     viewPager = findViewById(R.id.viewPager);
     ImageSwipeActivity imageSwipeActivity = new ImageSwipeActivity(this);
     viewPager.setAdapter(imageSwipeActivity);
+
+    Timer timer = new Timer();
+    timer.scheduleAtFixedRate(new myTimerTask(), 2000, 4000);
   }
 
 
@@ -39,6 +44,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
       case R.id.buttonLogin:
         startActivity(new Intent(this, LoginActivity.class));
         break;
+    }
+  }
+
+  public class myTimerTask extends TimerTask {
+
+    @Override
+    public void run() {
+      MainActivity.this.runOnUiThread(new Runnable() {
+        @Override
+        public void run() {
+          if (viewPager.getCurrentItem() == 0) {
+            viewPager.setCurrentItem(1);
+          } else if (viewPager.getCurrentItem() == 1) {
+            viewPager.setCurrentItem(2);
+          } else if (viewPager.getCurrentItem() == 2) {
+            viewPager.setCurrentItem(3);
+          } else {
+            viewPager.setCurrentItem(0);
+          }
+        }
+      });
     }
   }
 
