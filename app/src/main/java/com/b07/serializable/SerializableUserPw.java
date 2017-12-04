f079ab1766746aacdcc725efefc3c686b2f7ba05
chenxuyu <chenxuyu@UTORONTO.CA>
2017-12-04 18:59:54 +0000

    Added Serializable Package

---
+++ app/src/main/java/com/b07/serializable/SerializableUserPw.java
@@ -0,0 +1,37 @@
+package com.b07.serializable;
+
+import java.io.Serializable;
+
+public class SerializableUserPw {
+
+public class UserPwSerial implements Serializable {
+  
+  private static final long serialVersionUID = 8122331894936517645L;
+  private int userId;
+  private String userPw;
+  
+  private UserPwSerial(int userId, String userPw) {
+    this.setUserId(userId);
+    this.setUserPw(userPw);
+  }
+  
+  private void setUserId(int userId) {
+    this.userId = userId;
+  }
+  
+  @SuppressWarnings("unused")
+  private int getUserId() {
+    return this.userId;
+  }
+  
+  private void setUserPw(String userPw) {
+    this.userPw = userPw;
+  }
+  
+  @SuppressWarnings("unused")
+  private String getUserPw() {
+    return this.userPw;
+  }
+}
+
+}
