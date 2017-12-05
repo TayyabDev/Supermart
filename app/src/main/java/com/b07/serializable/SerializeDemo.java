0ac805ee9c00a29173c2c5ae1c86c165f40c4af6
chenxuyu <chenxuyu@UTORONTO.CA>
2017-12-05 01:46:35 +0000

    Fixed a type

--- app/src/main/java/com/b07/serializable/SerializeDemo.java
+++ app/src/main/java/com/b07/serializable/SerializeDemo.java
@@ -24,7 +24,7 @@ public class SerializeDemo {
     out.writeObject(obj);
     out.close();
     fileOut.close();
-    System.out.printf("Serialized data is saved in" + filepath);
+    System.out.println("Serialized data is saved in" + filepath);
   }
 
   /**
