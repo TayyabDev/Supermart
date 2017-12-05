4a5af4eae149522c7f9674b66dc26b5dce18ed52
waqartay <waqartay@UTORONTO.CA>
2017-12-05 00:56:55 +0000

    style guide for main acyivity

--- app/src/main/java/group0669/com/example/supermart/MainActivity.java
+++ app/src/main/java/group0669/com/example/supermart/MainActivity.java
@@ -5,13 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import com.b07.database.DatabaseDriverAndroid;
 import com.b07.database.helper.android.DatabaseAndroidSelectHelper;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -29,7 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     DatabaseAndroidSelectHelper sel = new DatabaseAndroidSelectHelper(this);
 
     // if no users in database then go to initialization page
-    if(sel.getUsersDetailsHelper().size() < 1){
+    if (sel.getUsersDetailsHelper().size() < 1) {
       startActivity(new Intent(this, InitializationActivity.class));
       finish();
     } else {
@@ -39,8 +36,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     /**
-    Timer timer = new Timer();
-    timer.scheduleAtFixedRate(new myTimerTask(), 4000, 4000);
+     Timer timer = new Timer();
+     timer.scheduleAtFixedRate(new myTimerTask(), 4000, 4000);
      */
   }
 
@@ -60,25 +57,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
   }
 
   /**
-  public class myTimerTask extends TimerTask {
+   public class myTimerTask extends TimerTask {
 
-    @Override
-    public void run() {
-      MainActivity.this.runOnUiThread(new Runnable() {
-        @Override
-        public void run() {
-          if (viewPager.getCurrentItem() == 0) {
-            viewPager.setCurrentItem(1);
-          } else if (viewPager.getCurrentItem() == 1) {
-            viewPager.setCurrentItem(2);
-          } else if (viewPager.getCurrentItem() == 2) {
-            viewPager.setCurrentItem(3);
-          } else {
-            viewPager.setCurrentItem(0);
-          }
-        }
-      });
-    }
+  @Override public void run() {
+  MainActivity.this.runOnUiThread(new Runnable() {
+  @Override public void run() {
+  if (viewPager.getCurrentItem() == 0) {
+  viewPager.setCurrentItem(1);
+  } else if (viewPager.getCurrentItem() == 1) {
+  viewPager.setCurrentItem(2);
+  } else if (viewPager.getCurrentItem() == 2) {
+  viewPager.setCurrentItem(3);
+  } else {
+  viewPager.setCurrentItem(0);
+  }
+  }
+  });
+  }
   }
    */
 
